444d858fb890f0428575cc6daac081f9bcc2035e
voblak2 <vinceoblak@vinces-mbp.lan>
2020-07-27 13:39:17 -0500

    Responsive and other minor changes

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
     <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php wp_head(); ?>
     </head>
-    <body>
+    <body <?php body_class(); ?>>
         <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
